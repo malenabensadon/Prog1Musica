6a9bca71df130153ad70b5c4a35e0a60b617533e
malenabensadon <bensadonm@udesa.edu.ar>
2021-06-03 18:34:39 -0300

    malee

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
+//Buscador
+
+let buscar = document.querySelector('form');
+
+buscar.addEventListener('click', function(){
+
+    if(buscar == ''){
+        alert('Necesitamos que escribas algo');
+        window.location.href = 
+    }else(buscar < '3')
+   
+});
+
+buscar.addEventListener('submit', function(){
+    alert('Necesitamos que escribas algo');
+    
+});
 
