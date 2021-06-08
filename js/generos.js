d94e6d2bde6997d6b53f1f6f279343454e26bbdc
malenabensadon <bensadonm@udesa.edu.ar>
2021-06-08 18:24:36 -0300

    male

---
+++ js/generos.js
@@ -0,0 +1,16 @@
+//api generos
+let url = 'https://developers.deezer.com/api/genre';
+
+fetch(url)
+    .then(function(info){
+        return info.json;
+    })
+    .then(function(data){
+        console.log(data);
+        let 
+
+
+    })
+
+
+
---
+++ js/index.js
@@ -0,0 +1,6 @@
+// capturas el form
+//event listener para submit //.length
+// prevent default y empezas a preguntar > un if si lo que vino en el campo busqueda es igual a ''
+//campo 
+
+let buscar 
