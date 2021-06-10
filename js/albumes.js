ee0e036c538480f22e4e70b83b61fb84e4390805
malenabensadon <bensadonm@udesa.edu.ar>
2021-06-10 15:16:29 -0300

    maleb

---
+++ js/albumes.js
@@ -0,0 +1,23 @@
+//ejemplo con un solo dato.
+let urlAlbum = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/302127";
+
+
+fetch(urlAlbum)
+    .then(function(response){
+        return response.json();
+    })
+
+    .then(function(data){
+        let album = data.results[0];
+     //destinos
+        let texto = document.querySelector('.texto');
+        let fotos = document.querySelector('.fotos');
+        let listadetallealbum = document.querySelector('.listadetallealbum');
+
+        texto.innerHTML += album.pag;
+        listadetallealbum.innerHTML += `${album.listas.last}. ${album.listas.first}. ${album.listas.last}`;
+        fotos.src = album.picture.medium;
+    })
+    .catch( function(error){
+   
+    })
