1d71bdb8801da83af79e882b0886c3f71fb0f9ce
justinabsegui <mbasombriosegui@udesa.edu.ar>
2021-06-11 12:51:40 -0300

    jshtmlindex

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+let proxy = 'https://cors-anywhere.herokuapp.com/'
 let topTracks = 'https://api.deezer.com/chart/0/tracks'
 let url = proxy+topTracks
 
@@ -13,49 +14,11 @@ fetch(url)
         let contenidoTrack= '';
 
         for(let i=0; i<info.kength; i++){
-            contenidoTrack += '<li class = "caja"> 
-                                        <a href="./detail-track.html?id=${info
-                                        [i].id }"><img class="fotos" src="$
-                                        {info[i].album.cover_medium}"
-                                        alt=""></a>
-                                        <a href="./detail-track.html"
-                                        class="names">${info[i].title}</a> <a
-                                        href=".playlists.html"></a>
-                                        <a href="./detail-artist.html"class="names">by Justin Bieber</a>
+            contenidoTrack += `<li class="caja"> 
+                                        <a href="./detail-track.html?id=${info[i].id }"><img class="fotos" src="${info[i].album.cover_medium}"
+                                        alt=""></a> <a href="./detail-track.html"class="names">${info[i].title}</a> <a href=".playlists.html"></a><a href="./detail-artist.html"class="names">by Justin Bieber</a>
                                         <a href="./playlists.html"><img class="favx"src="./img/fav.jpg"alt=""></a>
-                                </li>
+                                </li>`
         }
-        trackContainer.innerHTML += contenidoTrack                        
-                    
-// capturas el form
-//event listener para submit //.length
-// prevent default y empezas a preguntar > un if si lo que vino en el campo busqueda es igual a ''
-//campo 
-
-//creamos variable
-
-//capturamos el elemento y recibimos string
-//let bucar = document.querySelector();
-let buscar = document.querySelector('form')
-let campoBuscar = document.querySelector('[name="search"]')
-
-//creamos un evento
-buscar.addEventListener('submit', function(event){
-    event.preventDefault();
-    //condicinales
-    if(campoBuscar.value == ""){
-        //hago un alert
-        alert('El buscador esta vacio');
-    }else{
-        this.submit()
-    }
-    
-})
-
-//limpiar el mensaje de error cuando el usuario modifique el contenido del campo input. 
-campoBuscar.addEventListener('input', function(){
-    alert.innerText = "";
-})
-
-
-
+        trackContainer.innerHTML += contenidoTrack
+    })
--- readme.md
+++ readme.md
@@ -1,1 +1,32 @@
 Demo para subir a github
+
+
+// capturas el form
+//event listener para submit //.length
+// prevent default y empezas a preguntar > un if si lo que vino en el campo busqueda es igual a ''
+//campo 
+
+//creamos variable
+
+//capturamos el elemento y recibimos string
+//let bucar = document.querySelector();
+let buscar = document.querySelector('form')
+let campoBuscar = document.querySelector('[name="search"]')
+para index
+//creamos un evento
+buscar.addEventListener('submit', function(event){
+    event.preventDefault();
+    //condicinales
+    if(campoBuscar.value == ""){
+        //hago un alert
+        alert('El buscador esta vacio');
+    }else{
+        this.submit()
+    }
+    
+})
+
+//limpiar el mensaje de error cuando el usuario modifique el contenido del campo input. 
+campoBuscar.addEventListener('input', function(){
+    alert.innerText = "";
+})
