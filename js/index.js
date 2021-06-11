3485c053d9aafa997b36364311d34e71b0debf7f
justinabsegui <mbasombriosegui@udesa.edu.ar>
2021-06-11 12:37:27 -0300

    indexhtmljs

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
+let topTracks = 'https://api.deezer.com/chart/0/tracks'
+let url = proxy+topTracks
+
+fetch(url)
+    .then(function(response){
+        return response.json()
+
+    })
+    .then(function(data){
+        let info = data.data
+        console.log(info);
+        let trackContainer= document.querySelector('.track');
+        let contenidoTrack= '';
+
+        for(let i=0; i<info.kength; i++){
+            contenidoTrack += '<li class = "caja"> 
+                                        <a href="./detail-track.html?id=${info
+                                        [i].id }"><img class="fotos" src="$
+                                        {info[i].album.cover_medium}"
+                                        alt=""></a>
+                                        <a href="./detail-track.html"
+                                        class="names">${info[i].title}</a> <a
+                                        href=".playlists.html"></a>
+                                        <a href="./detail-artist.html"class="names">by Justin Bieber</a>
+                                        <a href="./playlists.html"><img class="favx"src="./img/fav.jpg"alt=""></a>
+                                </li>
+        }
+        trackContainer.innerHTML += contenidoTrack                        
+                    
 // capturas el form
 //event listener para submit //.length
 // prevent default y empezas a preguntar > un if si lo que vino en el campo busqueda es igual a ''
@@ -30,4 +59,3 @@ campoBuscar.addEventListener('input', function(){
 
 
 
-    
