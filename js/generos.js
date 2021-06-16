c55b40c56b73f78410061e500e038da038348dab
Maria Justina Basombrío Seguí <mbasombriosegui@udesa.edu.ar>
2021-06-16 15:34:06 -0300

    generos.js

--- js/generos.js
+++ js/generos.js
@@ -1,18 +1,29 @@
-//api generos
-let url = 'https://developers.deezer.com/api/genre';
+//url + proxy de generos
+let proxyAlbumes = 'https://cors-anywhere.herokuapp.com/'
+let GenerosPage = 'https://api.deezer.com/genre/'
+let urlGeneros = proxyGeneros+GenerosPage
 
-fetch(url)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data);
-        let 
+fetch(urlGeneros)
+.then(function(response){
+    return response.json()
 
+})
+.then(function(data){
+    let info = data.data
+    console.log(info);
+    let generosPageContainer= document.querySelector('.listas');
+    let contenidoGenerosPage= '';
 
-    })
-    .catch(function(data){
+    for(let i=0; i<info.length; i++){
 
-    })
-
-
+        contenidoGenerosPage +=  `<article class="genero">
+                                <a href="./detail-genres.html?id=${info[i].id}"><img class="fotos" src="${info[i].cover_medium}" alt=""></a>
+                                <h2 class="texto"><a href="./detail-genres.html" class="names">${info[i].title}</a></h2>
+                                </article>`
+               
+    }
+    generosPageContainer.innerHTML += contenidoGenerosPage
+})   
+.catch( function(error){
+    console.log(error);
+})
