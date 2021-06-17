14cbcc65caf885e3df48e30b043a9419fc5a61d4
haniaachaval <hachavalrodriguez@udesa.edu.ar>
2021-06-17 15:20:24 -0300

    cambioshania

--- js/detail-album.js
+++ js/detail-album.js
@@ -24,22 +24,40 @@ window.addEventListener('load', function() {
          image.src = data.contributors.cover_medium;
 
          let title = document.querySelector('.title');
-         //creamos ruta hacia el nombre del artista
+         //creamos ruta hacia el nombre del disco
          title.innetText = data.genres.data.title;
 
-         let artist = document.querySelector('.artist') = "Artista:";
+         let artist = document.querySelector('.artist') = 'Artista:';
          //creamos ruta hacia el nombre del artista
          artist.innerText = data.artist.name;
 
-         let genero = document.querySelector('.genre');
+         let genero = document.querySelector('.genre')='Genero:'
          //creamos ruta hacia el nombre del genero
          genero.innerText = data.genres.data.name;
+
+         let publicacion = document.querySelector('.date')= 'Publicacion:';
+         //fecha de publicacion 
+         publicacion.innerText = data.date 
+
+         //creamos lista d tracks dentro el album
+         let info = data.data
+         let albumesContainer= document.querySelector('.tracks');
+         let DetalleTracks= '';
+
+         //creamos un for que recorra las canciones del album
+             for(let i=0; i<10; i++){
+                    DetalleTracks +=   `<li>${info[i].tracks} </li>`
+                    }
+                    
+            albumesContainer.innerHTML += DetalleTracks
+            })
+
   
      })
      .catch( function(error){
          console.log (error);
      })
- })
+
   
 
  
