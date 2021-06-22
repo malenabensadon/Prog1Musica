4a780fc401a53e8e92f54f356bda2ee32a6f3316
malenabensadon <bensadonm@udesa.edu.ar>
2021-06-22 10:26:39 -0300

    male

--- js/search-results.js
+++ js/search-results.js
@@ -91,8 +91,8 @@ fetch(url2)
         for(let i=0; i<info.length; i++){
             contenidoTrack += `<li class="caja"> 
                                         <a href="./detail-track.html?id=${info[i].id }"><img class="fotos" src="${info[i].album.cover_medium}"
-                                        alt=""></a> <a href="./detail-track.html"class="names">${info[i].title}</a> <a href=".playlists.html"></a><a href="./detail-artist.html?id=${info[i].artist.id}"class="names">${info[i].artist.name}</a>
-                                        <a href="./playlists.html"><img class="favx"src="./img/fav.jpg"alt=""></a>
+                                        alt=""></a> <a href="./detail-track.html?id=${info[i].id}"class="names">${info[i].title}</a> <a href=".playlists.html"></a><a href="./detail-artist.html?id=${info[i].artist.id}"class="names">${info[i].artist.name}</a>
+                                        <a href="./detail-track.html?id=${info[i].id}"><img class="favx"src="./img/fav.jpg"alt=""></a>
                                 </li>`
         }
         //editamos nuestro html
